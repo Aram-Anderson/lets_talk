ee8d9cdb4fd8162408aacdd64fc08ebc27798eb0
Aram Anderson <aram.j.anderson@gmail.com>
2017-12-26 16:53:31 -0700

    Add procfile

--- lib/lets_talk_web/channels/user_socket.ex
+++ lib/lets_talk_web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule LetsTalkWeb.UserSocket do
   # channel "room:*", LetsTalkWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
